2f87c17b1ca2cac51ce845ff24c6437c1d829a95
Ellie <elliewem@gmail.com>
2018-02-13 18:30:01 +0000

    Refactor note-list-model to use IIFE

--- view/index.js
+++ view/index.js
@@ -1,5 +1,8 @@
-notelist = new NoteList();
-noteListView = new NoteListView(notelist);
-noteController = new NoteController(notelist, noteListView);
-noteController.add("Favourite drink: seltzer")
-noteController.insertHTML();
+(function() {
+    notelist = new NoteList();
+    noteListView = new NoteListView(notelist);
+    noteController = new NoteController(notelist, noteListView);
+    noteController.add("Favourite drink: seltzer")
+    noteController.insertHTML();
+  }
+)();
